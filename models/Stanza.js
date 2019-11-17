04ae13248c479d5982300e84dbc37e5d0006487e
Johnny Perkins <johnny@mantl.com>
2019-11-17 18:13:43 -0500

    Update now.json

--- models/Stanza.js
+++ models/Stanza.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose')
 
 const StanzaSchema = new mongoose.Schema(
-	{
-		section: { type: mongoose.Schema.ObjectId, ref: 'Section' },
-		poem: { type: mongoose.Schema.ObjectId, ref: 'Poem' },
-		leadWord: String,
-		body: String,
-	},
-	{ timestamps: true },
+  {
+    section: { type: mongoose.Schema.ObjectId, ref: 'Section' },
+    poem: { type: mongoose.Schema.ObjectId, ref: 'Poem' },
+    leadWord: String,
+    body: String
+  },
+  { timestamps: true }
 )
 
 module.exports = mongoose.model('Stanza', StanzaSchema)
